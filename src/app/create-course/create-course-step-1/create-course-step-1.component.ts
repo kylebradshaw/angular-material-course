1c3148d1d82df07ba91af607e9e4559cdaba5fa4
Kyle Bradshaw <kbradshaw@abiomed.com>
2023-06-27 11:57:31 -0400

    17 textarea

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {UntypedFormBuilder, Validators} from '@angular/forms';
 import {MatCalendarCellClassFunction} from '@angular/material/datepicker';
 
+const SAMPLE_TEXT = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ';
+
 @Component({
   selector: "create-course-step-1",
   templateUrl:"create-course-step-1.component.html",
@@ -19,7 +21,7 @@ export class CreateCourseStep1Component {
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: [SAMPLE_TEXT, [Validators.required, Validators.minLength(3)]]
   });
 
   dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
