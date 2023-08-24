d470594da7934ca33ff8e7ff0645db91be1784e1
Kyle Bradshaw <kbradshaw@abiomed.com>
2023-08-23 20:48:23 -0400

    35. Responsive BreakpointObserver A++

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Course} from "../model/course";
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
 import { filter } from 'rxjs/operators';
@@ -12,14 +13,42 @@ export class CoursesCardListComponent implements OnInit {
 
     @Input()
     courses: Course[];
+    cols = 3;
+    rowHeight = '500px';
 
     constructor(
       private dialog: MatDialog,
+      private responsive: BreakpointObserver,
     ) {
     }
 
     ngOnInit() {
+      // example of how the service works
+      // this.responsive.observe(['(max-width: 959px)']).subscribe(result => console.log(result));
 
+      // usually used like this
+      this.responsive.observe([
+        Breakpoints.TabletPortrait,
+        Breakpoints.TabletLandscape,
+        Breakpoints.HandsetPortrait,
+        Breakpoints.HandsetLandscape,
+      ]).subscribe(result => {
+        console.log(result)
+        this.cols = 3;
+        this.rowHeight = '500px';
+        // we can change the display appropriately
+        const breakpoints = result.breakpoints;
+        if (breakpoints[Breakpoints.TabletPortrait]) {
+          this.cols = 1;
+        } else if (breakpoints[Breakpoints.HandsetPortrait]) {
+           this.cols = 1;
+           this.rowHeight = '430px';
+        } else if (breakpoints[Breakpoints.HandsetLandscape]) {
+          this.cols = 1;
+        } else if (breakpoints[Breakpoints.TabletLandscape] || breakpoints[Breakpoints.WebLandscape]) {
+          this.cols = 2;
+        }
+      });
     }
 
     editCourse(course:Course) {
